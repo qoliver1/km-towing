9e05177f7b5fcd93d3cfac35b40d55a042f25ee7
qoliver1 <qoliver1@gmail.com>
2026-08-08 09:50:46 -0500

    Add isolated K&M Towing test deployment

--- src/worker.ts
+++ src/worker.ts
@@ -8,7 +8,7 @@ export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url)
 
-    url.pathname = url.pathname.replace(/^\/kmtow/, '') || '/'
+    url.pathname = url.pathname.replace(/^\/(kmtow-test-page|kmtow)/, '') || '/'
 
     return env.ASSETS.fetch(new Request(url, request))
   },
