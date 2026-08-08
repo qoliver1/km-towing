e4737911e551aba64663c1e93a980b7d4c6fb414
qoliver1 <qoliver1@gmail.com>
2026-08-08 07:20:39 -0500

    getting rdy for gemz.com/kmtow

---
+++ src/worker.ts
@@ -0,0 +1,15 @@
+interface Env {
+  ASSETS: {
+    fetch(request: Request): Promise<Response>
+  }
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url)
+
+    url.pathname = url.pathname.replace(/^\/kmtow/, '') || '/'
+
+    return env.ASSETS.fetch(new Request(url, request))
+  },
+}
